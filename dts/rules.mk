f9ac51a946b397f8af899c32c397677b11a0a912
Ultracoolguy <ultracoolguy@disroot.org>
2021-07-03 12:15:17 -0400

    Revert "dts: Add motorola-ocean (SDM632)"
    This reverts commit 069609ebc2dde8b9149453768093374ecf5a3747.
    motorola-ocean is now in the master-sdm632, so it does nothing here
    other than cause confusion.

--- dts/rules.mk
+++ dts/rules.mk
@@ -16,7 +16,6 @@ DTBS += \
 	$(LOCAL_DIR)/msm8953-xiaomi-mido.dtb \
 	$(LOCAL_DIR)/msm8953-xiaomi-vince.dtb \
 	$(LOCAL_DIR)/msm8953-xiaomi-tissot.dtb \
-	$(LOCAL_DIR)/sdm632-motorola-ocean.dtb \
 	$(LOCAL_DIR)/msm8953-xiaomi-daisy.dtb
 endif
 ifeq ($(PROJECT), msm8952-secondary)
